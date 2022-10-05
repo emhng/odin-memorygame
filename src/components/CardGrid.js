0bb5f4da33371902618c1e2abe6510f08d80b5a5
emhng <contactemilyhuang@gmail.com>
2022-10-04 17:07:42 -0700

    Adjust condition for randomizing emojis

--- src/components/CardGrid.js
+++ src/components/CardGrid.js
@@ -1,5 +1,5 @@
-import '../styles/CardGrid.css';
-import Card from '../components/Card';
+import '../styles/CardGrid.css'
+import Card from '../components/Card'
 
 const CardGrid = ({ onClick, clickedEmojis }) => {
   const emojiList = [
@@ -117,55 +117,55 @@ const CardGrid = ({ onClick, clickedEmojis }) => {
     '🙀',
     '😿',
     '😾'
-  ];
+  ]
 
-  const generatedCards = [];
+  const generatedCards = []
 
   const randomIndex = array => {
-    return Math.floor(Math.random() * array.length);
-  };
+    return Math.floor(Math.random() * array.length)
+  }
 
   const randomLength = () => {
-    return Math.floor(Math.random() * (15 - 0 + 1) + 0);
-  };
+    return Math.floor(Math.random() * (15 - 0 + 1) + 0)
+  }
 
-  const length = randomLength();
+  const length = randomLength()
 
   const randomEmoji = () => {
-    let index, randomEmoji;
+    let index, randomEmoji
 
     if (clickedEmojis.length >= 16 && generatedCards.length === length) {
       // Make sure to include one unclicked emoji in a random position
       const unclickedEmojis = emojiList.filter(
         emoji => clickedEmojis.indexOf(emoji) === -1
-      );
+      )
 
       if (unclickedEmojis.length === 0) {
-        alert('You clicked all emojis only once!');
+        alert('You clicked all emojis only once!')
+      } else {
+        index = randomIndex(unclickedEmojis)
+        randomEmoji = unclickedEmojis[index]
       }
-
-      index = randomIndex(unclickedEmojis);
-      randomEmoji = unclickedEmojis[index];
     } else {
-      index = randomIndex(emojiList);
-      randomEmoji = emojiList[index];
+      index = randomIndex(emojiList)
+      randomEmoji = emojiList[index]
 
       //Remove emoji from array so it cannot be picked twice
-      emojiList.splice(index, 1);
+      emojiList.splice(index, 1)
     }
 
-    return randomEmoji;
-  };
+    return randomEmoji
+  }
 
   for (let i = 0; i < 16; i++) {
-    generatedCards.push(<Card emoji={randomEmoji()} key={i} />);
+    generatedCards.push(<Card emoji={randomEmoji()} key={i} />)
   }
 
   return (
     <div onClick={onClick} id='card-grid'>
       {generatedCards}
     </div>
-  );
-};
+  )
+}
 
-export default CardGrid;
+export default CardGrid
